34b830b279e9d304d0260b18348eb9a66129dacf
vitalys <pumych@gmail.com>
2014-08-17 11:12:29 +0300

    qunit added (async need to be tested)

---
+++ 2.testing_and_debugging/tests.js
@@ -0,0 +1,9 @@
+QUnit.test( "hello test", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+    assert(false, "Fail!");
+});
+
+QUnit.test( "New test", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+    assert(false, "Fail!");
+});
